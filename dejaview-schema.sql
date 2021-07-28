c35cd03ae2285a32095524efd7fcc26c15ae04ed
Kathrin Neyzberg <kneyzberg@gmail.com>
2021-07-27 18:19:11 -0700

    database changes

--- dejaview-schema.sql
+++ dejaview-schema.sql
@@ -1,31 +1,31 @@
 CREATE TABLE users (
-  username VARCHAR(25) PRIMARY KEY CHECK,
+  username VARCHAR(25) PRIMARY KEY,
   name TEXT NOT NULL,
   email TEXT NOT NULL
     CHECK (position('@' IN email) > 1),
-  password TEXT NOT NULL,
+  password TEXT NOT NULL
 );
 
 CREATE TABLE lists (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
-  user_id REFERENCES users DELETE ON CASCADE
+  user_username TEXT NOT NULL REFERENCES users ON DELETE CASCADE
 );
 
 CREATE TABLE movies (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   year INTEGER NOT NULL, 
-  image_url TEXT,
+  image_url TEXT
 );
 
 CREATE TABLE moviesandlists(
-  list_id REFERENCES lists NOT NULL, 
-  movie_id REFERENCES movies NOT NULL,
-)
+  list_id INTEGER NOT NULL REFERENCES lists , 
+  movie_id INTEGER NOT NULL REFERENCES movies
+);
 
 CREATE TABLE ratings(
-  user_id REFERENCES users NOT NULL, 
-  movie_id REFERENCES movies NOT NULL,
+  user_username VARCHAR(25) NOT NULL REFERENCES users ON DELETE CASCADE, 
+  movie_id INTEGER NOT NULL REFERENCES movies,
   rating NUMERIC NOT NULL
-)
+);
--- dejaview.sql
+++ dejaview.sql
@@ -6,7 +6,7 @@ CREATE DATABASE dejaview;
 \connect dejaview
 
 \i dejaview-schema.sql
-\i dejaview-seed.sql
+-- \i dejaview-seed.sql
 
 \echo 'Delete and recreate dejaview_test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
